32baad37310370366f417e757176ff3cb532f4e3
White-Devil-boy <70854919+White-Devil-boy@users.noreply.github.com>
2020-09-11 14:14:28 +0530

    Update and rename suruBomb.sh to TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[4;31m Suru Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
 echo -e "\e[1;32m TBomb \e[0m"
 echo "Press Enter To Continue"
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By Divyanshu >update.speedx
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -25,12 +25,12 @@ rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
 figlet TBomb
-echo -e "\e[1;34m Created By \e[1;32m"
+echo -e "\e[1;34m Created By Divyanshu\e[1;32m"
 toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+echo -e "\e[4;34m This Bomber Was Created By Divyanshu \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: vinaynishad507@gmail.com \e[0m"
+echo -e "\e[4;32m   YouTube Page:                          \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -50,7 +50,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By SURU's Hack'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -90,9 +90,9 @@ echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
 echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
+echo "         [-] Mail At: vinaynishad507@gmail.com"
 echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
-echo "         [-] Ping At: http://wa.me/917600140353"
+echo "         [-] Ping At: http://wa.me/917080637784"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
 echo "         [-] Ping At: https://t.me/Rieltar"
 echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
@@ -106,9 +106,9 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: vinaynishad507@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://chat.whatsapp.com/C3Xf0xjrES2JLTG6eycvSp \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/channel/UCtQHEryVa2lrb-CLqn1ikFA \e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
@@ -117,11 +117,11 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border SURU
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: vinaynishad507@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp:  https://chat.whatsapp.com/C3Xf0xjrES2JLTG6eycvSp\e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/channel/UCtQHEryVa2lrb-CLqn1ikFA \e[0m"
 echo " "
 exit 0
 else
